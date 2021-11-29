c889f67acd0ae809f801dc930b91ff4e286498d3
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-11-29 13:48:33 +0100

    Update to global loop

--- src/ResumeResponseBodyMiddleware.php
+++ src/ResumeResponseBodyMiddleware.php
@@ -6,7 +6,7 @@ namespace WyriHaximus\React\Http\Middleware;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use React\EventLoop\LoopInterface;
+use React\EventLoop\Loop;
 use React\Promise\PromiseInterface;
 use React\Stream\ReadableStreamInterface;
 
@@ -14,21 +14,14 @@ use function React\Promise\resolve;
 
 final class ResumeResponseBodyMiddleware
 {
-    private LoopInterface $loop;
-
-    public function __construct(LoopInterface $loop)
-    {
-        $this->loop = $loop;
-    }
-
     public function __invoke(ServerRequestInterface $request, callable $next): PromiseInterface
     {
-        return resolve($next($request))->then(function (ResponseInterface $response): ResponseInterface {
+        return resolve($next($request))->then(static function (ResponseInterface $response): ResponseInterface {
             $body = $response->getBody();
 
             if ($body instanceof ReadableStreamInterface) {
                 /** @psalm-suppress InvalidArgument */
-                $this->loop->futureTick([$body, 'resume']);
+                Loop::futureTick([$body, 'resume']);
             }
 
             return $response;
--- tests/ResumeResponseBodyMiddlewareTest.php
+++ tests/ResumeResponseBodyMiddlewareTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace WyriHaximus\React\Tests\Http\Middleware;
 
-use Prophecy\Argument;
-use React\EventLoop\LoopInterface;
+use Psr\Http\Message\ResponseInterface;
+use React\EventLoop\Loop;
 use React\Http\Io\HttpBodyStream;
 use React\Http\Message\Response;
 use RingCentral\Psr7\ServerRequest;
@@ -22,23 +22,16 @@ final class ResumeResponseBodyMiddlewareTest extends AsyncTestCase
      */
     public function resume(): void
     {
-        $loop = $this->prophesize(LoopInterface::class);
-        $loop->futureTick(Argument::that(static function (array $args): bool {
-            $args[0]->{$args[1]}(); /** @phpstan-ignore-line */
-
-            return true;
-        }))->shouldBeCalled();
-
         $body = $this->prophesize(HttpBodyStream::class);
         $body->resume()->shouldBeCalled();
 
         $response = (new Response())->withBody($body->reveal());
 
-        $next = static function () use ($response) {
-            return $response;
-        };
+        $next = static fn (): ResponseInterface => $response;
 
-        $middleware = new ResumeResponseBodyMiddleware($loop->reveal());
+        $middleware = new ResumeResponseBodyMiddleware();
         $middleware(new ServerRequest('GET', 'https://example.com/'), $next);
+
+        Loop::run();
     }
 }
